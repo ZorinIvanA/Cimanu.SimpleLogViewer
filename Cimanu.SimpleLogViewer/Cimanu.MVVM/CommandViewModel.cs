6f7d0e839d803c37f3adb1ed10cf8a8fa33e102c
Зорин Иван Андреевич <zorin.ivan@outlook.com>
2018-03-22 22:39:21 +0300

    Старт проекта

---
+++ Cimanu.SimpleLogViewer/Cimanu.MVVM/CommandViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Cimanu.MVVM
+{
+    public class CommandViewModel : ICommand
+    {
+        protected Action<Object> action { get; }
+        protected Func<object, bool> canExecute { get; }
+        public event EventHandler<bool> Executed;
+
+        public CommandViewModel(Action<Object> action, Func<object, bool> canExecute = null)
+        {
+            if (action == null)
+                throw new ArgumentException("Action is null!");
+
+            this.action = action;
+
+            this.canExecute = (canExecute == null) ? ((x) => { return true; }) : canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            var commandTask = Task.Run(() => { action(parameter); });
+        }
+    }
+}
